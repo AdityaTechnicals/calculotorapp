925634b4f03319d0c77dec5a0643a9e52a72b387
Aditya8957 <86321648+Aditya8957@users.noreply.github.com>
2022-04-12 20:53:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,131 @@
 import React,{useState,useEffect} from 'react'
 import './App.css';
+import NumberFormat from 'react-number-format';
 
 
 function App() {
+  
   const [input, setinput] = useState("0");
   const [prestate, setprestate] = useState("");
   const [curstate, setcurstate] = useState("");
   const [oper,setoper] = useState(null);
   const [total, settotal] = useState(false);
+  const [style, setstyle] = useState({
+    backgroundColor:"#283747",    
+  })
+  const [CALSTYLE, setCALSTYLE] = useState({
+    backgroundColor:"#ECF0F1"
+  });
+  const drk =() =>{
+
+    if(style.backgroundColor==="#283747"){
+      setstyle({
+        backgroundColor:"#ECF0F1"
+      })
+      setCALSTYLE({
+        backgroundColor:"#283747"
+      })
+    }else{
+      setstyle({
+        backgroundColor:"#283747"
+      })
+      setCALSTYLE({
+        backgroundColor:"#ECF0F1"
+      })
+    }
+  }
   const inputNum =(e) =>{
     if(total){
-      setprestate("")
+      setprestate("");
     }
-    curstate ? setcurstate(pre=>pre + e.target.innerText ) :setcurstate(e.target.innerText)
-    settotal(false)
-  }
+    curstate ? setcurstate( pre=>pre + e.target.innerText ) : setcurstate(e.target.innerText)
+    settotal(false);
+  };
   useEffect(() => {
   setinput(curstate)
   }, [curstate]);
   useEffect(() => {
     setinput("0")
   }, []);
-  const opera =(w) =>{
-
+  const opera =(e) =>{
+    settotal(false);
+setoper(e.target.innerText)
+if(curstate==="0") return
+if(prestate !== ""){
+  equal()
+}else{
+setprestate(curstate)
+setcurstate("");
+};
   }
+  
   const reset =() =>{
-
+   setinput("0")
+   setprestate("")
+   setcurstate("")
   }
-  const equal =() =>{
-
+  const equal =(e) =>{
+   if(e?.target.innerText==="="){
+   settotal(true);   }
+  
+  let cal
+  switch(oper){
+    case "/": 
+      cal =String(parseFloat(prestate)/parseFloat(curstate));
+      break;
+      case "X": 
+      cal =String(parseFloat(prestate)*parseFloat(curstate));
+      break;
+      case "+": 
+      cal =String(parseFloat(prestate)+parseFloat(curstate));
+      break;
+      case "-": 
+          cal = String(parseFloat(prestate)-parseFloat(curstate));
+      break; 
+      default: 
+      return;
   }
-
+  setinput("")
+  setprestate(cal)
+  setcurstate("")
+  }
   return (
 
 
-   <>
-   
- <div className="container">
+   <div className="body" style={style}>
+   <div className="header">
+     <h1 >Calculatorapp <span>Light Theme</span></h1>
+   </div>
+ <div className="container" style={CALSTYLE}>
 <div className="wrapper">
-  <div className="screen"> {input} </div>
+  <div className="screen"> {input !=="" || input ==="0" ? <NumberFormat value={input} displayType={'text'}/>:<NumberFormat value={prestate} displayType={'text'}/>} </div>
     <div className="btn light-gray" onClick={reset}>AC</div>
     <div className="btn org" onClick={opera}>-</div>
 
     <div className="btn org" onClick={opera}>/</div>
     <div className="btn org" onClick={opera}>X</div>
-    <div className="btn" onclick={inputNum}>7</div>
-    <div className="btn" onclick={inputNum}>8</div>
-    <div className="btn" onclick={inputNum}>9</div>
+    <div className="btn" onClick={inputNum} value="7">7</div>
+    <div className="btn" onClick={inputNum} value="8">8</div>
+    <div className="btn" onClick={inputNum} value="9">9</div>
     <div className="btn org" onClick={opera}>+</div>
-    <div className="btn" onclick={inputNum}>4</div>
-    <div className="btn" onclick={inputNum}>5</div>
-    <div className="btn" onclick={inputNum}>6</div>
+    <div className="btn" onClick={inputNum} value="4">4</div>
+    <div className="btn" onClick={inputNum} value="5">5</div>
+    <div className="btn" onClick={inputNum} value="6">6</div>
     <div className="btn org" onClick={equal}>=</div>
-    <div className="btn" onclick={inputNum}>1</div>
-    <div className="btn" onclick={inputNum}>2</div>
-    <div className="btn" onclick={inputNum}>3</div>
+    <div className="btn" onClick={inputNum} value="1">1</div>
+    <div className="btn" onClick={inputNum} value="2">2</div>
+    <div className="btn" onClick={inputNum} value="3">3</div>
+    <div className="btn" onClick={inputNum} value="0">0</div>
 
 
 
 
 </div>
  </div>
-   </>
+ <footer>
+   <button onClick={drk}>Enable theme</button>
+ </footer>
+   </div>
   );
 }
 
