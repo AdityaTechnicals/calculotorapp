d73815200fb0e544a560cbe418db21cbbf94fe58
Aditya8957 <86321648+Aditya8957@users.noreply.github.com>
2022-04-12 10:33:18 +0530

    idfc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
 import './App.css';
 
+
 function App() {
+  const [input, setinput] = useState("0");
+  const [prestate, setprestate] = useState("");
+  const [curstate, setcurstate] = useState("");
+  const [oper,setoper] = useState(null);
+  const [total, settotal] = useState(false);
+  const inputNum =(e) =>{
+    if(total){
+      setprestate("")
+    }
+    curstate ? setcurstate(pre=>pre + e.target.innerText ) :setcurstate(e.target.innerText)
+    settotal(false)
+  }
+  useEffect(() => {
+  setinput(curstate)
+  }, [curstate]);
+  useEffect(() => {
+    setinput("0")
+  }, []);
+  const opera =(w) =>{
+
+  }
+  const reset =() =>{
+
+  }
+  const equal =() =>{
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+   <>
+   
+ <div className="container">
+<div className="wrapper">
+  <div className="screen"> {input} </div>
+    <div className="btn light-gray" onClick={reset}>AC</div>
+    <div className="btn org" onClick={opera}>-</div>
+
+    <div className="btn org" onClick={opera}>/</div>
+    <div className="btn org" onClick={opera}>X</div>
+    <div className="btn" onclick={inputNum}>7</div>
+    <div className="btn" onclick={inputNum}>8</div>
+    <div className="btn" onclick={inputNum}>9</div>
+    <div className="btn org" onClick={opera}>+</div>
+    <div className="btn" onclick={inputNum}>4</div>
+    <div className="btn" onclick={inputNum}>5</div>
+    <div className="btn" onclick={inputNum}>6</div>
+    <div className="btn org" onClick={equal}>=</div>
+    <div className="btn" onclick={inputNum}>1</div>
+    <div className="btn" onclick={inputNum}>2</div>
+    <div className="btn" onclick={inputNum}>3</div>
+
+
+
+
+</div>
+ </div>
+   </>
   );
 }
 
